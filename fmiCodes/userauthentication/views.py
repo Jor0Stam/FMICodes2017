12c1a00f1a437810502ad8614844996a3c84e975
Panayot Panayotov <p.d.panayotov09@gmail.com>
2017-12-16 22:07:11 +0200

    feat: Add userauthrntication view.

--- fmiCodes/userauthentication/views.py
+++ fmiCodes/userauthentication/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+# from django.http import HttpResponse
+# from alexmateva.models import User
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
 
-# Create your views here.
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            email = form.cleaned_data.get('email')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(email=email, password=raw_password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'signup.html', {'form': form})
+
+
+# def login(request):
+#     m = User.objects.get(username=request.POST['username'])
+#     if m.password == request.POST['password']:
+#         request.session['member_id'] = m.id
+#         return HttpResponse("You're logged in.")
+#     else:
+#         return HttpResponse("Your username and password didn't match.")
