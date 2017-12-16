b5ac26e1218c4ad5bb9634061b0a22fe3ee77dc0
Jor0Stam <g.a.stamenov@gmail.com>
2017-12-16 17:53:21 +0200

    Create get create-event form

--- fmiCodes/alexmateva/models.py
+++ fmiCodes/alexmateva/models.py
@@ -1,13 +1,12 @@
 from django.db import models
 from django.utils import timezone
-from 
 # Create your models here.
 
 class Event(models.Model):
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=255)
     created_date = models.DateTimeField(default=timezone.now())
     description = models.TextField()
-    creator = models.ForeignKey(User, editable = False)
+    # creator = models.ForeignKey(User, editable = False)
     rating = models.IntegerField()
-    location = models.CharField()
+    location = models.CharField(max_length=255)
     #pics
--- fmiCodes/alexmateva/views.py
+++ fmiCodes/alexmateva/views.py
@@ -1,12 +1,25 @@
 from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponse
+from .models import Event
+from .forms import EventForm
 
 class EventCreater(View):
-    def post(self, request, *args, **kwargs):
-        form = self.form_class(request.POST)
+    template_name = 'alexmateva/create-event.html'
+
+    def get(self, request):
+        form = EventForm(request.POST)
+
         if form.is_valid():
-            # <process form cleaned data>
+
             return HttpResponseRedirect('/success/')
 
         return render(request, self.template_name, {'form': form})
+
+    def post(self, request, *args, **kwargs):
+
+        if form.is_valid():
+
+            return HttpResponseRedirect('/success/')
+
+        return render(request, self.template_name, locals())
