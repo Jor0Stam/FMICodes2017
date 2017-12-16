1fbefc91d566f1619357d459a7ca61dadd3e2a0b
simeon <SimeonAlexandrov>
2017-12-16 16:32:51 +0200

    Add model event.

--- fmiCodes/alexmateva/models.py
+++ fmiCodes/alexmateva/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
+from django.utils import timezone
+from 
 # Create your models here.
+
+class Event(models.Model):
+    name = models.CharField(max_length=200)
+    created_date = models.DateTimeField(default=timezone.now())
+    description = models.TextField()
+    creator = models.ForeignKey(User, editable = False)
+    rating = models.IntegerField()
+    location = models.CharField()
+    #pics
