afdb9ff2ec5c272f69701d6373dcb55992ca9e86
Jor0Stam <g.a.stamenov@gmail.com>
2017-12-17 08:36:23 +0200

    almost ready crud for events

--- fmiCodes/alexmateva/urls.py
+++ fmiCodes/alexmateva/urls.py
@@ -1,7 +1,8 @@
-from django.urls import include, path
-from .views import EventCreater, EventLister
+from django.urls import include, path, re_path
+from .views import EventCreater, EventLister, EventDetail
 
 urlpatterns = [
     path('create-event', EventCreater.as_view(), name='list_events'),
-    path('list-events', EventLister.as_view(), name='list_events')
+    path('list-events', EventLister.as_view(), name='list_events'),
+    re_path('event/(?P<event_id>[0-9])', EventDetail.as_view(), name='event_details'),
 ]
