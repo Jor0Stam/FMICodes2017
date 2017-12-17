d92eefe28e58888b8a4c25836cb8a516bdce62a0
Panayot Panayotov <p.d.panayotov09@gmail.com>
2017-12-17 15:13:43 +0200

    style: Update identitations.

--- fmiCodes/challenges/views.py
+++ fmiCodes/challenges/views.py
@@ -1,16 +1,18 @@
 from django.shortcuts import render
 from django.views import View
-from django.views.generic import ListView,DetailView
+from django.views.generic import ListView, DetailView
 from django.http import HttpResponse
 from .models import Challenge
 from .forms import ChallengeForm
 
 # Create your views here.
 
+
 class ChallengeDetail(DetailView):
 	model = Challenge
 	def get_context_data(self,*args):
 		pass
+
 
 class ChallengeCreator(View):
 	template_name = 'challenges/create_challenge.html'
@@ -23,18 +25,20 @@ class ChallengeCreator(View):
 
 		return render(request,self.template_name,{'form':form})
 
+
 	def post(self,request,*args,**kwargs):
 		form = ChallengeForm(request.POST)
 
 		if form.is_valid():
 			Challenge.objects.create(name=request.POST.get('name'),
-									description=request.POST.get('description'),
-									events = request.POST.get('events'))
-			
+									 description=request.POST.get('description'),
+									 events = request.POST.get('events'))
+
 			return redirect('challenges_list')
 
-		
+
 		return render(request,self.template_name,locals())
+
 
 class ChallengeLister(ListView):
 
@@ -42,9 +46,6 @@ class ChallengeLister(ListView):
     template_name = 'challenges/challenges_list.html'
 
     def get(self, request):
+		hallenges = Challenge.objects.all()
 
-    	challenges = Challenge.objects.all()
     	return render(request,self.template_name,locals())
-
-
-
