c67cac702ab87b69ffe1291b4b13b240a6aa8ca3
AlexMateva <alex.mateva@gmail.com>
2017-12-17 04:20:17 +0200

    add login template view

--- fmiCodes/userauthentication/urls.py
+++ fmiCodes/userauthentication/urls.py
@@ -3,5 +3,6 @@ from django.contrib import admin
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('login', auth_views.login, {'template_name': 'login.html'}, name='login'),
+    path('login', auth_views.login, name='login'),
+    # path('registration', auth_views.registration, name='registration'),
 ]
